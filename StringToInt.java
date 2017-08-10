1b37275c393abbb92060743856703c3d1961d85b
Vigneshutta <gchitradevi97@gmail.com>
2017-08-10 18:15:41 +0530

    Create StringToInt.java

---
+++ StringToInt.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+public class StringToInt
+{
+   public static void main(String[] args)
+   {
+      
+      String a="5";
+      int num=Integer.parseInt(a);
+      System.out.println(num);
+      }
+      } 
