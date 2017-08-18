40d369879e1ee1cf061ec66fa05795ff3aaa77ee
Vigneshutta <gchitradevi97@gmail.com>
2017-08-18 09:45:03 +0530

    Create Concat.java

---
+++ Concat.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Concat{
+ 
+        public static void main(String[] args) {
+        String s1="devi";
+        String s2="Kavi";
+        String s3=s1+s2;
+        System.out.println(s3);
+        }
+        }
