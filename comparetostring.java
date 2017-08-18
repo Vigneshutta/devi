7c4860c82202bd49b1bfd0c12e1b25710742b9ad
Vigneshutta <gchitradevi97@gmail.com>
2017-08-18 09:30:42 +0530

    Create comparetostring.java

---
+++ comparetostring.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+class comparetostring {
+  
+  public static void main(String args[]) {
+      String s1="Chitra";
+      String s2="Vignesh";
+      System.out.println(s1.equals(s2));
+    }
+    }
